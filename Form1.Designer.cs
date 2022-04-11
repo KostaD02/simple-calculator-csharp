a7dbc40823b1472cd7bdefac083019939b21be74
Kosta <68782786+KostaD02@users.noreply.github.com>
2022-04-11 15:25:19 +0400

    initial code

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kdDisplayArea = new System.Windows.Forms.TextBox();
+            this.kdButtonOne = new System.Windows.Forms.Button();
+            this.kdButtonTwo = new System.Windows.Forms.Button();
+            this.kdButtonThree = new System.Windows.Forms.Button();
+            this.kdButtonFour = new System.Windows.Forms.Button();
+            this.kdButtonFive = new System.Windows.Forms.Button();
+            this.kdButtonSix = new System.Windows.Forms.Button();
+            this.kdButtonSeven = new System.Windows.Forms.Button();
+            this.kdButtonEight = new System.Windows.Forms.Button();
+            this.kdButtonNine = new System.Windows.Forms.Button();
+            this.kdButtonPlus = new System.Windows.Forms.Button();
+            this.kdButtonMinus = new System.Windows.Forms.Button();
+            this.kdButtonMultiply = new System.Windows.Forms.Button();
+            this.kdButtonDivision = new System.Windows.Forms.Button();
+            this.kdButtonEqual = new System.Windows.Forms.Button();
+            this.kdButtonZero = new System.Windows.Forms.Button();
+            this.kdButtonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // kdDisplayArea
+            // 
+            this.kdDisplayArea.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdDisplayArea.Location = new System.Drawing.Point(13, 12);
+            this.kdDisplayArea.Name = "kdDisplayArea";
+            this.kdDisplayArea.ReadOnly = true;
+            this.kdDisplayArea.Size = new System.Drawing.Size(268, 34);
+            this.kdDisplayArea.TabIndex = 0;
+            this.kdDisplayArea.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // kdButtonOne
+            // 
+            this.kdButtonOne.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonOne.Location = new System.Drawing.Point(13, 57);
+            this.kdButtonOne.Name = "kdButtonOne";
+            this.kdButtonOne.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonOne.TabIndex = 1;
+            this.kdButtonOne.Text = "1";
+            this.kdButtonOne.UseVisualStyleBackColor = true;
+            this.kdButtonOne.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // kdButtonTwo
+            // 
+            this.kdButtonTwo.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonTwo.Location = new System.Drawing.Point(82, 57);
+            this.kdButtonTwo.Name = "kdButtonTwo";
+            this.kdButtonTwo.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonTwo.TabIndex = 1;
+            this.kdButtonTwo.Text = "2";
+            this.kdButtonTwo.UseVisualStyleBackColor = true;
+            this.kdButtonTwo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // kdButtonThree
+            // 
+            this.kdButtonThree.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonThree.Location = new System.Drawing.Point(153, 57);
+            this.kdButtonThree.Name = "kdButtonThree";
+            this.kdButtonThree.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonThree.TabIndex = 1;
+            this.kdButtonThree.Text = "3";
+            this.kdButtonThree.UseVisualStyleBackColor = true;
+            this.kdButtonThree.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // kdButtonFour
+            // 
+            this.kdButtonFour.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonFour.Location = new System.Drawing.Point(13, 119);
+            this.kdButtonFour.Name = "kdButtonFour";
+            this.kdButtonFour.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonFour.TabIndex = 1;
+            this.kdButtonFour.Text = "4";
+            this.kdButtonFour.UseVisualStyleBackColor = true;
+            this.kdButtonFour.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // kdButtonFive
+            // 
+            this.kdButtonFive.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonFive.Location = new System.Drawing.Point(82, 119);
+            this.kdButtonFive.Name = "kdButtonFive";
+            this.kdButtonFive.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonFive.TabIndex = 1;
+            this.kdButtonFive.Text = "5";
+            this.kdButtonFive.UseVisualStyleBackColor = true;
+            this.kdButtonFive.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // kdButtonSix
+            // 
+            this.kdButtonSix.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonSix.Location = new System.Drawing.Point(153, 119);
+            this.kdButtonSix.Name = "kdButtonSix";
+            this.kdButtonSix.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonSix.TabIndex = 1;
+            this.kdButtonSix.Text = "6";
+            this.kdButtonSix.UseVisualStyleBackColor = true;
+            this.kdButtonSix.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // kdButtonSeven
+            // 
+            this.kdButtonSeven.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonSeven.Location = new System.Drawing.Point(13, 182);
+            this.kdButtonSeven.Name = "kdButtonSeven";
+            this.kdButtonSeven.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonSeven.TabIndex = 1;
+            this.kdButtonSeven.Text = "7";
+            this.kdButtonSeven.UseVisualStyleBackColor = true;
+            this.kdButtonSeven.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // kdButtonEight
+            // 
+            this.kdButtonEight.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonEight.Location = new System.Drawing.Point(82, 182);
+            this.kdButtonEight.Name = "kdButtonEight";
+            this.kdButtonEight.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonEight.TabIndex = 1;
+            this.kdButtonEight.Text = "8";
+            this.kdButtonEight.UseVisualStyleBackColor = true;
+            this.kdButtonEight.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // kdButtonNine
+            // 
+            this.kdButtonNine.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonNine.Location = new System.Drawing.Point(153, 182);
+            this.kdButtonNine.Name = "kdButtonNine";
+            this.kdButtonNine.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonNine.TabIndex = 1;
+            this.kdButtonNine.Text = "9";
+            this.kdButtonNine.UseVisualStyleBackColor = true;
+            this.kdButtonNine.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // kdButtonPlus
+            // 
+            this.kdButtonPlus.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonPlus.Location = new System.Drawing.Point(227, 57);
+            this.kdButtonPlus.Name = "kdButtonPlus";
+            this.kdButtonPlus.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonPlus.TabIndex = 1;
+            this.kdButtonPlus.Text = "+";
+            this.kdButtonPlus.UseVisualStyleBackColor = true;
+            this.kdButtonPlus.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // kdButtonMinus
+            // 
+            this.kdButtonMinus.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonMinus.Location = new System.Drawing.Point(227, 119);
+            this.kdButtonMinus.Name = "kdButtonMinus";
+            this.kdButtonMinus.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonMinus.TabIndex = 1;
+            this.kdButtonMinus.Text = "-";
+            this.kdButtonMinus.UseVisualStyleBackColor = true;
+            this.kdButtonMinus.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // kdButtonMultiply
+            // 
+            this.kdButtonMultiply.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonMultiply.Location = new System.Drawing.Point(227, 182);
+            this.kdButtonMultiply.Name = "kdButtonMultiply";
+            this.kdButtonMultiply.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonMultiply.TabIndex = 1;
+            this.kdButtonMultiply.Text = "*";
+            this.kdButtonMultiply.UseVisualStyleBackColor = true;
+            this.kdButtonMultiply.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // kdButtonDivision
+            // 
+            this.kdButtonDivision.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonDivision.Location = new System.Drawing.Point(227, 244);
+            this.kdButtonDivision.Name = "kdButtonDivision";
+            this.kdButtonDivision.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonDivision.TabIndex = 1;
+            this.kdButtonDivision.Text = "/";
+            this.kdButtonDivision.UseVisualStyleBackColor = true;
+            this.kdButtonDivision.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // kdButtonEqual
+            // 
+            this.kdButtonEqual.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonEqual.Location = new System.Drawing.Point(152, 244);
+            this.kdButtonEqual.Name = "kdButtonEqual";
+            this.kdButtonEqual.Size = new System.Drawing.Size(55, 38);
+            this.kdButtonEqual.TabIndex = 1;
+            this.kdButtonEqual.Text = "=";
+            this.kdButtonEqual.UseVisualStyleBackColor = true;
+            this.kdButtonEqual.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // kdButtonZero
+            // 
+            this.kdButtonZero.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonZero.Location = new System.Drawing.Point(13, 244);
+            this.kdButtonZero.Name = "kdButtonZero";
+            this.kdButtonZero.Size = new System.Drawing.Size(54, 38);
+            this.kdButtonZero.TabIndex = 1;
+            this.kdButtonZero.Text = "0";
+            this.kdButtonZero.UseVisualStyleBackColor = true;
+            this.kdButtonZero.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // kdButtonClear
+            // 
+            this.kdButtonClear.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.kdButtonClear.Location = new System.Drawing.Point(81, 244);
+            this.kdButtonClear.Name = "kdButtonClear";
+            this.kdButtonClear.Size = new System.Drawing.Size(55, 38);
+            this.kdButtonClear.TabIndex = 1;
+            this.kdButtonClear.Text = "Clear";
+            this.kdButtonClear.UseVisualStyleBackColor = true;
+            this.kdButtonClear.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 311);
+            this.Controls.Add(this.kdButtonNine);
+            this.Controls.Add(this.kdButtonEight);
+            this.Controls.Add(this.kdButtonSeven);
+            this.Controls.Add(this.kdButtonSix);
+            this.Controls.Add(this.kdButtonFive);
+            this.Controls.Add(this.kdButtonFour);
+            this.Controls.Add(this.kdButtonThree);
+            this.Controls.Add(this.kdButtonTwo);
+            this.Controls.Add(this.kdButtonZero);
+            this.Controls.Add(this.kdButtonClear);
+            this.Controls.Add(this.kdButtonEqual);
+            this.Controls.Add(this.kdButtonDivision);
+            this.Controls.Add(this.kdButtonMultiply);
+            this.Controls.Add(this.kdButtonMinus);
+            this.Controls.Add(this.kdButtonPlus);
+            this.Controls.Add(this.kdButtonOne);
+            this.Controls.Add(this.kdDisplayArea);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox kdDisplayArea;
+        private System.Windows.Forms.Button kdButtonOne;
+        private System.Windows.Forms.Button kdButtonTwo;
+        private System.Windows.Forms.Button kdButtonThree;
+        private System.Windows.Forms.Button kdButtonFour;
+        private System.Windows.Forms.Button kdButtonFive;
+        private System.Windows.Forms.Button kdButtonSix;
+        private System.Windows.Forms.Button kdButtonSeven;
+        private System.Windows.Forms.Button kdButtonEight;
+        private System.Windows.Forms.Button kdButtonNine;
+        private System.Windows.Forms.Button kdButtonPlus;
+        private System.Windows.Forms.Button kdButtonMinus;
+        private System.Windows.Forms.Button kdButtonMultiply;
+        private System.Windows.Forms.Button kdButtonDivision;
+        private System.Windows.Forms.Button kdButtonEqual;
+        private System.Windows.Forms.Button kdButtonZero;
+        private System.Windows.Forms.Button kdButtonClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        string inputedValue = "0";
+        string firstNumber = "0";
+        string secondNumber = "0";
+        string operators = "";
+        private void display(string symbol)
+        {
+                kdDisplayArea.Text += symbol;   
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "1";
+            }
+            else
+            {
+                inputedValue = "1";
+            }
+            display("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "2";
+            }
+            else
+            {
+                inputedValue = "2";
+            }
+            display("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "3";
+            }
+            else
+            {
+                inputedValue = "3";
+            }
+            display("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "4";
+            }
+            else
+            {
+                inputedValue = "4";
+            }
+            display("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "5";
+            }
+            else
+            {
+                inputedValue = "5";
+            }
+            display("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "6";
+            }
+            else
+            {
+                inputedValue = "6";
+            }
+            display("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "7";
+            }
+            else
+            {
+                inputedValue = "7";
+            }
+            display("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "8";
+            }
+            else
+            {
+                inputedValue = "8";
+            }
+            display("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "9";
+            }
+            else
+            {
+                inputedValue = "9";
+            }
+            display("9");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            firstNumber = inputedValue;
+            inputedValue = "0";
+            operators = "+";
+            display(" + ");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            firstNumber = inputedValue;
+            inputedValue = "0";
+            operators = "-";
+            display(" - ");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            firstNumber = inputedValue;
+            inputedValue = "0";
+            operators = "*";
+            display(" * ");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            firstNumber = inputedValue;
+            inputedValue = "0";
+            operators = "/";
+            display(" / ");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            secondNumber = inputedValue;
+            if (operators != "")
+            {
+                double number1;
+                double number2;
+                double result;
+                try
+                {
+                    number1 = Convert.ToDouble(firstNumber);
+                    number2 = Convert.ToDouble(secondNumber);
+                    switch (operators)
+                    {
+                        case "+":
+                            {
+                                kdDisplayArea.Text = "";
+                                result = number1 + number2;
+                                kdDisplayArea.Text = result.ToString();
+                                inputedValue = result.ToString();
+                                break;
+                            }
+                        case "-":
+                            {
+                                kdDisplayArea.Text = "";
+                                result = number1 - number2;
+                                kdDisplayArea.Text = result.ToString();
+                                inputedValue = result.ToString();
+                                break;
+                            }
+                        case "*":
+                            {
+                                kdDisplayArea.Text = "";
+                                result = number1 * number2;
+                                kdDisplayArea.Text = result.ToString();
+                                inputedValue = result.ToString();
+                                break;
+                            }
+                        case "/":
+                            {
+                                kdDisplayArea.Text = "";
+                                if (number2 == 0)
+                                {
+                                    kdDisplayArea.Text = "Infinity";
+                                }
+                                else
+                                {
+                                    result = number1 / number2;
+                                    kdDisplayArea.Text = result.ToString();
+                                    inputedValue = result.ToString();
+                                }
+                                break;
+                            }
+                    }
+                }
+                catch
+                {
+                    kdDisplayArea.Text = "Math error";
+                    inputedValue = "";
+                    firstNumber = "";
+                    secondNumber = "";
+                    operators = "";
+                }
+            }
+            else
+            {
+                kdDisplayArea.Text = "Use buttons :)";
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (inputedValue != "0")
+            {
+                inputedValue += "0";
+                display("0");
+            }
+            else
+            {
+                if (firstNumber == "0")
+                {
+                    inputedValue = "0";
+                }
+                else
+                {
+                    inputedValue += "0";
+                }
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            kdDisplayArea.Text = "";
+            inputedValue = "";
+            firstNumber = "";
+            secondNumber = "";
+            operators = "";
+        }
+    }
+}
